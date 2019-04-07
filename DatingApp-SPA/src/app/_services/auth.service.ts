bf8cb1d143630daad2bf167d63b51d806b0b4bef
Sreenivasulu Chitlam <schitlam@gmail.com>
2019-04-07 18:40:46 -0400

    Changes

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
-import {map} from "rxjs/operators"
+import { map } from "rxjs/operators";
+import { JwtHelperService } from '@auth0/angular-jwt';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  jwtHealtper = new JwtHelperService();
+  decodedToken:any;
   baseUtl = "http://locahost:5000/api/auth/";
   constructor(private http: HttpClient) { }
   httpOptions :any = {
@@ -14,8 +18,6 @@ export class AuthService {
     })
   };
   login(model: any) {
-
-    
     var url = this.baseUtl + "login";
     return this.http.post("http://localhost:5000/api/auth/login",
       model, this.httpOptions)
@@ -24,11 +26,18 @@ export class AuthService {
           const user = response;
           if (user) {
             localStorage.setItem("token", user.token);
+            this.decodedToken = this.jwtHealtper.decodeToken(user.token);
+            console.log(this.decodedToken);
           }
         }));
   }
 
   register(model: any) {
     return this.http.post("http://localhost:5000/api/auth/register", model, this.httpOptions);
+  }
+
+  loggedIn() {
+    const token = localStorage.getItem("token");
+    return !this.jwtHealtper.isTokenExpired(token);
   }
 }
